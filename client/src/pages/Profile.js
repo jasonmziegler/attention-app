02655b993eb7247b26f59792e3ac9767932f8965
Jason Ziegler <jason@insightfulautomation.com>
2024-09-08 18:04:05 -0500

    Update Profile with User data

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
 
 function Profile() {
+  const {user} = useContext(AuthContext);
+
   return (
-    <h2>Profile Page</h2>
+    <div>
+      <h2>Profile Page</h2>
+      <p><strong>Username:</strong> {user.username}</p>
+      <p><strong>Email:</strong> {user.email}</p>
+      <p><strong>User ID:</strong> {user._id}</p>
+    </div>
   )
 }
 
