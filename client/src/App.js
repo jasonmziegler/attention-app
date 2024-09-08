55e8e09db646087c21f6e7acb78347eeea084535
Jason Ziegler <jason@insightfulautomation.com>
2024-09-08 17:48:34 -0500

    Update App to protect profile route

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,61 @@
 // import logo from './logo.svg';
 // import './App.css';
-import React, {useContext} from 'react';
-import { BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
+import React, { useContext } from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import { AuthContext } from './context/AuthContext';
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import ProtectedRoute from "./components/ProtectedRoute";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
-  const {user, logout } = useContext(AuthContext);
-  
+  const { user, logout } = useContext(AuthContext);
+
   return (
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Attention App
-        </p>
+        <p>Attention App</p>
       </header>
       <Router>
         <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          {user ? (
-            <>
-              <li>
-                <Link to="/profile">Profile</Link>
-              </li>
-              <li>
-                <button onClick={logout}>Logout</button>
-              </li>
-            </>
-          ) : (
-            <>
-              <li>
-                <Link to="/signup">Signup</Link>
-              </li>
-              <li>
-                <Link to="/login">Login</Link>
-              </li>
-            </>
-          )}
-        </ul>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            {user ? (
+              <>
+                <li>
+                  <Link to="/profile">Profile</Link>
+                </li>
+                <li>
+                  <button onClick={logout}>Logout</button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link to="/signup">Signup</Link>
+                </li>
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+              </>
+            )}
+          </ul>
         </nav>
         <div>
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/profile" element={<Profile />} />
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
             <Route path="/signup" element={<Signup />} />
             <Route path="/login" element={<Login />} />
           </Routes>
