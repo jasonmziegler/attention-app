c9c8f227245ff823d5483a5337f946106e47ed53
Jason Ziegler <jason@insightfulautomation.com>
2024-09-08 16:29:28 -0500

    Update Login.js

---
+++ client/src/pages/Login.js
@@ -0,0 +1,53 @@
+// src/pages/Login.js
+import React, { useState, useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
+
+function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) {
+    e.preventDefault();
+    const res = await login(email, password);
+    if (res.success) {
+      navigate('/profile');
+    } else {
+      setError(res.message);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      {error && <p style={{color: 'red'}}>{error}</p>}
+      {/*TODO write handleChange function*/}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input 
+            type='email'
+            value={email}
+            onChange={(e) =>setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input 
+            type='password'
+            value={password}
+            onChange={(e) =>setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type='submit'>Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
