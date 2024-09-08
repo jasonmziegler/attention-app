491c907c03ee1589c617d0c3f29981581d523c51
Jason Ziegler <jason@insightfulautomation.com>
2024-09-08 16:59:04 -0500

    Update App.js with Signup and Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 // import logo from './logo.svg';
 // import './App.css';
+import React, {useContext} from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import { AuthContext } from './context/AuthContext';
 
 function App() {
+  const {user, logout } = useContext(AuthContext);
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -14,10 +20,38 @@ function App() {
         </p>
       </header>
       <Router>
+        <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          {user ? (
+            <>
+              <li>
+                <Link to="/profile">Profile</Link>
+              </li>
+              <li>
+                <button onClick={logout}>Logout</button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link to="/signup">Signup</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </>
+          )}
+        </ul>
+        </nav>
         <div>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/profile" element={<Profile />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
         </div>
       </Router>
